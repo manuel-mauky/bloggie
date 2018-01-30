0de29296f3745761d0514a6d19a1c594a7ae6456
Manuel Mauky <manuel.mauky@gmail.com>
2018-01-31 00:30:15 +0100

    implement mutations

--- backends/nodejs_apollo_server/data/resolvers.js
+++ backends/nodejs_apollo_server/data/resolvers.js
@@ -1,6 +1,47 @@
 import { Article, Author, Tag, Comment } from "./connectors"
+import { Op } from "sequelize"
+
+import R from "ramda"
 
 const resolvers = {
+  Mutation: {
+    addTag(root, args) {
+      return Tag.create({ name: args.name })
+    },
+
+    addArticle(root, args) {
+      console.log("args:", args)
+      return Article.create(args.input)
+    },
+
+    editArticle(root, args) {
+      return Article.findOne({ where: { id: args.id } }).then(article => {
+        const other = R.omit(args, "id", "tags")
+
+        return article.update(other)
+      })
+    },
+
+    editTags(root, args) {
+      return Article.findOne({ where: { id: args.articleId } }).then(article => {
+        if (article) {
+          return Tag.findAll({
+            where: {
+              id: {
+                [Op.in]: args.tags,
+              },
+            },
+          }).then(tags => {
+            return article.setTags(tags).then(() => {
+              return article
+            })
+          })
+        } else {
+          return article
+        }
+      })
+    },
+  },
   Query: {
     articles(root, args) {
       return Article.findAll()

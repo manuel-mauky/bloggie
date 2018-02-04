7cad33de1afa47f884748b764fc7a953fbbe4d08
Manuel Mauky <manuel.mauky@gmail.com>
2018-02-05 00:07:06 +0100

    add bootstrap, change port of backend server

--- backends/nodejs_apollo_server/server.js
+++ backends/nodejs_apollo_server/server.js
@@ -1,22 +1,22 @@
-import express from 'express'
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
-import bodyParser from 'body-parser'
-import schema from './data/schema'
+import express from "express"
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express"
+import bodyParser from "body-parser"
+import schema from "./data/schema"
 
-import { createTestData } from './data/testdata'
+import { createTestData } from "./data/testdata"
 
-const GRAPHQL_PORT = 3000
+const GRAPHQL_PORT = 4000
 
-const environment = process.env.NODE_ENV || 'development'
-if (environment === 'development') {
-    createTestData()
+const environment = process.env.NODE_ENV || "development"
+if (environment === "development") {
+  createTestData()
 }
 
 const graphQLServer = express()
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+graphQLServer.use("/graphql", bodyParser.json(), graphqlExpress({ schema }))
+graphQLServer.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }))
 
 graphQLServer.listen(GRAPHQL_PORT, () =>
-    console.log(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`)
+  console.log(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`)
 )
--- frontends/react-apollo/src/index.js
+++ frontends/react-apollo/src/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import "bootstrap/dist/css/bootstrap.css"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import registerServiceWorker from "./registerServiceWorker"
+
+ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()

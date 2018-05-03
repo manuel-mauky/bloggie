b753fa871a0fa9849622543c857c0d3a54acc3cf
Manuel Mauky <manuel.mauky@saxsys.de>
2018-05-03 19:06:52 +0200

    configured apollo-angular

--- frontends/angular-apollo/src/app/app.module.ts
+++ frontends/angular-apollo/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser"
 import { NgModule } from "@angular/core"
 
+import { HttpClientModule } from "@angular/common/http"
+import { ApolloModule, Apollo } from "apollo-angular"
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http"
+import { InMemoryCache } from "apollo-cache-inmemory"
+
 import { ArticlesModule } from "./articles/articles.module"
 import { AuthorsModule } from "./authors/authors.module"
 import { TagsModule } from "./tags/tags.module"
@@ -14,8 +19,24 @@ import { RoutingModule } from "./routing.module"
 
 @NgModule({
   declarations: [AppComponent, NavigationComponent, AboutComponent, NoMatchPageComponent],
-  imports: [BrowserModule, RoutingModule, ArticlesModule, AuthorsModule, TagsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
+    RoutingModule,
+    ArticlesModule,
+    AuthorsModule,
+    TagsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({ uri: "/api/graphql" }),
+      cache: new InMemoryCache(),
+    })
+  }
+}
--- frontends/angular-apollo/src/app/authors/authors-overview-page/authors-overview-page.component.ts
+++ frontends/angular-apollo/src/app/authors/authors-overview-page/authors-overview-page.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Apollo} from 'apollo-angular'
+import gql from 'graphql-tag'
+
+const authorListQuery = gql`
+  query authorList {
+    authors {
+      id
+      name
+      articles {
+        id
+        title
+        permalink
+        releaseDate
+      }
+    }
+  }
+`
+
+
+
+
 @Component({
   selector: 'app-authors-overview-page',
   templateUrl: './authors-overview-page.component.html',
   styleUrls: ['./authors-overview-page.component.css']
 })
-export class AuthorsOverviewPageComponent implements OnInit {
+export class AuthorsOverviewPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  loading: boolean,
+
+  authors: Array<any>
+
+  private querySubscription;
+
+
+  constructor(private apollo: Apollo) { }
 
   ngOnInit() {
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: authorListQuery
+    })
+    .valueChanges
+    .subscribe(({data, loading}) => {
+      this.loading = loading,
+      this.authors = data.authors
+    })
+
+  }
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe()
   }
 
 }

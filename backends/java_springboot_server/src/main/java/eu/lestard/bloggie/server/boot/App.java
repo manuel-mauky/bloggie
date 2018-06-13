72d6ee084028e9809616af25e5901304df2310fb
Manuel Mauky <manuel.mauky@saxsys.de>
2018-06-13 17:12:10 +0200

    Setup GraphQL hello world

--- backends/java_springboot_server/src/main/java/eu/lestard/bloggie/server/boot/App.java
+++ backends/java_springboot_server/src/main/java/eu/lestard/bloggie/server/boot/App.java
@@ -5,6 +5,10 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import graphql.Scalars;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
 
 @SpringBootApplication
 public class App {
@@ -14,6 +18,20 @@ public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class);
+	}
+
+	@Bean
+	GraphQLSchema schema() {
+		return GraphQLSchema.newSchema()
+				.query(GraphQLObjectType.newObject()
+						.name("query")
+						.field(field -> field
+								.name("test")
+								.type(Scalars.GraphQLString)
+								.dataFetcher(environment -> "hallo welt")
+						)
+						.build()
+				).build();
 	}
 
 	@PostConstruct
